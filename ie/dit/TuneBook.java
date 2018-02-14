d8b4d78bb07a9e1046f393617cfd61b6984d08b5
Povilas <povkub@gmail.com>
2018-02-14 17:08:00 +0000

    Trying to make the file reader work, keep getting a file not found error

--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -1,10 +1,47 @@
 package ie.dit;
 import java.util.ArrayList;
+import java.io.*;
 
 public class TuneBook
 {
+
+	ArrayList<Tune> tunes = new ArrayList<Tune>();
+
     public static void main(String[] args)
     {
-    	ArrayList<Tune> tunes = new ArrayList<Tune>();
+    	String fileName = "hnj0.abc";
+    	TuneBook tb = new TuneBook(fileName);
+    	System.out.println(tb);
     }
+
+    public TuneBook(String fileName)
+    {
+    	BufferedReader inputStream = null;
+		try {
+		    inputStream = new BufferedReader(new FileReader(fileName));
+		    String l;
+		    Tune newTune = new Tune()
+		    while ((l = inputStream.readLine()) != null) 
+		    {
+		        System.out.println(l);
+		    }
+		}
+		catch (IOException e)
+		{
+		    e.printStackTrace();
+		} 
+		finally 
+		{
+		    if (inputStream != null) {
+		        try
+		        {
+		            inputStream.close();
+		        }
+		        catch(Exception e)
+		        {
+		            e.printStackTrace();
+		        }
+		    }
+		}    
+	}
 }
