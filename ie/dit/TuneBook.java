7a140a700ab071bc3541271d6b6dc622b2eb351c
Povilas <povkub@gmail.com>
2018-02-14 16:40:17 +0000

    crerated ArrayList

--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -1,9 +1,10 @@
 package ie.dit;
+import java.util.ArrayList;
 
 public class TuneBook
 {
     public static void main(String[] args)
     {
-
+    	ArrayList<Tune> tunes = new ArrayList<Tune>();
     }
 }
