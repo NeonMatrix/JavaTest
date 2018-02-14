b65694603f0ae06861bc7eeb6addb1fdb0b59444
Povilas <povkub@gmail.com>
2018-02-14 16:23:03 +0000

    Made first java classes

---
+++ ie/dit/TuneBook.java
@@ -0,0 +1,9 @@
+package ie.dit;
+
+public class TuneBook
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ ie/dit/Tunes.java
@@ -0,0 +1,26 @@
+package ie.dit;
+
+public class Tunes
+{
+    int x;
+    String title;
+    String altTitle;
+    String notation;
+
+    // Accessor
+    public int getX()
+    {
+        return x;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAltTitle()
+    {
+        return altTitle;
+    }
+}
+}
